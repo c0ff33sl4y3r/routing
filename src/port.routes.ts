dbae6205df88403c72f2e922fe03ab0741f37338
jaibtdou <iamnoob2210@gmail.com>
2023-11-18 17:59:05 +0700

    Changed src

--- src/port.routes.ts
+++ src/port.routes.ts
@@ -1,8 +1,13 @@
 import { routes } from '@stricjs/app';
 import { text, json } from '@stricjs/app/send';
+import { qs } from '@stricjs/utils';
 
 export async function main() {
-    return routes('/api')
-    // Handle `/api` instead of `/`
-    .get('/', () => new Response('Hi'));
+    return routes('/:port').get('', c => {
+        const port = c.params.port;
+        const json = c.json();
+        console.log(qs.parse(qs.string(c)), json)
+        return text(`Hello from port ${port}`);
+})
 }
+
